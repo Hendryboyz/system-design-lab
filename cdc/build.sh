e245aa61c27334c06e306d562f47acc2215b7d37
Hendryboyz <hendry.js1247@gmail.com>
2024-07-27 17:54:24 +0800

    ci: build/deploy app scripts

--- cdc/build.sh
+++ cdc/build.sh
@@ -8,8 +8,21 @@ echo "==== Start to build api image version: $VERSION_NUMBER ===="
 
 docker build -t "dsebd-demo-api:$VERSION_NUMBER" api/
 docker image tag "dsebd-demo-api:$VERSION_NUMBER" "dsebd-demo-api:latest"
+docker image tag "dsebd-demo-api:$VERSION_NUMBER" "hendryboyz/demo-api-dsebd:$VERSION_NUMBER"
+docker image tag "dsebd-demo-api:$VERSION_NUMBER" hendryboyz/demo-api-dsebd:latest
 
 docker image ls | grep dsebd-demo-api
+
+echo "Push image dsebd-demo-api:$VERSION_NUMBER to docker? (Y/n)"
+
+read IS_PUSH
+
+if [ $(echo $IS_PUSH | tr '[:upper:]' '[:lower:]') = 'y' ]; then
+  docker push hendryboyz/demo-api-dsebd:latest
+  docker push "hendryboyz/demo-api-dsebd:$VERSION_NUMBER"
+  echo "dsebd-demo-api:$VERSION_NUMBER is pushed"
+fi
+
 
 echo "Delete image dsebd-demo-api:$VERSION_NUMBER? (Y/n)"
 
---
+++ cdc/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SOURCE_DIR=api/
+
+VERSION_NUMBER=$(grep version $SOURCE_DIR/package.json | head -1 | awk -F: '{print $2}' | sed 's/[\",]//g' | tr -d '[[:space:]]')
+
+echo "==== Start to deploy api image version: $VERSION_NUMBER ===="
+
+pushd k8s/app/overlay/dev/
+kustomize edit set image app_image="hendryboyz/demo-api-dsebd:$VERSION_NUMBER"
+kustomize build | kubectl apply -f -
+# kustomize build > deploy.yaml
+popd
